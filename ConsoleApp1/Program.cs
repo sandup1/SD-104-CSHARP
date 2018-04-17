791577ea721ae35a9163d87dbf03eafe6957f245
sandup <sundup_lama@yahoo.com>
2018-04-16 20:13:28 -0700

    COMPLETE ASSIGHNMENT

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double hits = Int32.Parse(Console.ReadLine());
+            double bats = Int32.Parse(Console.ReadLine());
+
+            string player = Console.ReadLine();
+
+            double Average = hits / bats;
+            Console.WriteLine(Average);
+
+        }
+
+    }
+}
